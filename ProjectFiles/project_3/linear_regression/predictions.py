e45166f13677098e57406c2a8adf34c9365b45fa
jbaker8935 <farmerjohn1958@comcast.net>
2015-04-06 13:15:01 -0400

    Committed by Udacity

---
+++ ProjectFiles/project_3/linear_regression/predictions.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import numpy as np
+from ggplot import *
+
+
+def normalize_features(df):
+    """
+    Normalize the features in our data set.
+    """
+    array_normalized = (df - df.mean()) / df.std()
+    mu = df.mean()
+    sigma = df.std()
+    if (sigma == 0).any():
+        raise Exception("One or more features had the same value for all samples, and thus could " + \
+                        "not be normalized. Please do not include features with only a single value " + \
+                        "in your model.")
+    return array_normalized, mu, sigma
+
+
+def compute_cost(features, values, theta):
+    """
+    Compute the cost function given a set of features / values,
+    and the values for our thetas.
+
+    This can be the same code as the compute_cost function in the lesson #3 exercises,
+    but feel free to implement your own.
+    """
+
+    # your code here
+    m = len(values)
+    sum_of_square_errors = np.square(np.dot(features, theta) - values).sum()
+    cost = sum_of_square_errors / (2 * m)
+
+    return cost
+
+
+def gradient_descent(features, values, theta, alpha, num_iterations):
+    """
+    Perform gradient descent given a data set with an arbitrary number of features.
+    
+    This is the same gradient descent code as in the lesson #3 exercises. But feel free
+    to implement your own.
+    """
+    m = len(values)
+    cost_history = []
+
+    for i in range(num_iterations):
+        predicted = np.dot(features,theta)
+        diff = predicted - values
+        clause = np.dot(diff,features) / len(values)
+        theta = theta - alpha * clause
+        cost_history.append(compute_cost(features, values, theta))
+
+    return theta, pd.Series(cost_history)
+
+
+def predictions(dataframe):
+    '''
+    The NYC turnstile data is stored in a pd dataframe called weather_turnstile.
+    Using the information stored in the dataframe, lets predict the ridership of
+    the NYC subway using linear regression with gradient descent.
+    
+    You can look at information contained in the turnstile weather dataframe 
+    at the link below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
+    
+    Your prediction should have a R^2 value of .40 or better.
+    
+    Note: due to the memory and CPU limitation of our amazon EC2 instance, we will
+    give you a random subet (~15%) of the data contained in turnstile_data_master_with_weather.csv
+    
+    If you receive a "server has encountered an error" message, that means you are hitting 
+    the 30 second  limit that's placed on running your program. Try using a smaller number
+    for num_iterations if that's the case.
+    
+    Or if you are using your own algorithm/modesl, see if you can optimize your code so it
+    runs faster.
+    '''
+
+    dummy_units = pd.get_dummies(dataframe['UNIT'], prefix='unit')
+    features = dataframe[['rain', 'precipi', 'Hour', 'meantempi']].join(dummy_units)
+    values = dataframe[['ENTRIESn_hourly']]
+    m = len(values)
+
+    features, mu, sigma = normalize_features(features)
+
+    features['ones'] = np.ones(m)
+    features_array = np.array(features)
+    values_array = np.array(values).flatten()
+
+    # Set values for alpha, number of iterations.
+    alpha = 0.2 # please feel free to play with this value
+    num_iterations = 25  # please feel free to play with this value
+
+    #Initialize theta, perform gradient descent
+    theta_gradient_descent = np.zeros(len(features.columns))
+    theta_gradient_descent, cost_history = gradient_descent(features_array, values_array, theta_gradient_descent,
+                                                            alpha, num_iterations)
+
+    plot = None
+    plot = plot_cost_history(alpha, cost_history)
+    print(plot)
+
+    predictions = np.dot(features_array, theta_gradient_descent)
+
+    return predictions
+
+
+def compute_r_squared(data, predictions):
+    SST = ((data - np.mean(data)) ** 2).sum()
+    SSReg = ((predictions - np.mean(data)) ** 2).sum()
+    r_squared = SSReg / SST
+
+    return r_squared
+
+def plot_cost_history(alpha, cost_history):
+   """This function is for viewing the plot of your cost history.
+   You can run it by uncommenting this
+
+       plot_cost_history(alpha, cost_history)
+
+   call in predictions.
+
+   If you want to run this locally, you should print the return value
+   from this function.
+   """
+   cost_df = pd.DataFrame({
+      'Cost_History': cost_history,
+      'Iteration': range(len(cost_history))
+   })
+   return ggplot(cost_df, aes('Iteration', 'Cost_History')) + \
+      geom_point() + ggtitle('Cost History for alpha = %.3f' % alpha )
+
+if __name__ == "__main__":
+    input_filename = "turnstile_data_master_with_weather.csv"
+    turnstile_master = pd.read_csv(input_filename)
+    predicted_values = predictions(turnstile_master)
+    r_squared = compute_r_squared(turnstile_master['ENTRIESn_hourly'], predicted_values)
+
+    print(r_squared)
